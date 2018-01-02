8475f5ade619a7ac173b8d7102762f1afa4fe566
Josh Frankel <joshmfrankel@gmail.com>
2018-01-02 11:54:18 -0500

    Enable sign-up via Clearance
    * Overridden sign-up form to account for required :name column

--- app/controllers/academics_controller.rb
+++ app/controllers/academics_controller.rb
@@ -1,2 +1,13 @@
 class AcademicsController < ApplicationController
+  def create
+    Academic.create(academic_params[:academic])
+
+    redirect_back fallback_location: root_path
+  end
+
+  private
+
+  def academic_params
+    params.permit(academic: [:name, :email, :password])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "journals#index"
+
   resources :journals
   resources :academics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
