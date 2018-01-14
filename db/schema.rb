19bdc890aa6170608d96e2da10cd3e0d9e0f4d4f
Josh Frankel <joshmfrankel@gmail.com>
2018-01-13 22:37:17 -0500

    Add pg extension to schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180102160547) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "academics", force: :cascade do |t|
     t.string "name", null: false
     t.string "title"
