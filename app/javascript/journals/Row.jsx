a0527d36c6ac78d2839f3c47f3f5456ada398f32
Josh Frankel <joshmfrankel@gmail.com>
2018-03-30 06:52:44 -0400

    Add binary sorting to journals, Begin edit mode for rows

--- app/javascript/journals/Row.jsx
+++ app/javascript/journals/Row.jsx
@@ -1,14 +1,34 @@
 import React from "react"
 
 class Row extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isEditing: false };
+
+    this.toggleEditMode = this.toggleEditMode.bind(this);
+  }
+
+  toggleEditMode(e) {
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+
+  displayRowData(data) {
+    if (this.state.isEditing) {
+      return <input value={data} />;
+    }
+    return data;
+  }
+
   render() {
     const journal = this.props.journal;
+
     return (
       <tr>
-        <td>{journal.title}</td>
-        <td>{journal.description}</td>
-        <td>{journal.impact_factor}</td>
-        <td>Edit</td>
+        <td>{this.displayRowData(journal.title)}</td>
+        <td>{this.displayRowData(journal.description)}</td>
+        <td>{this.displayRowData(journal.impact_factor)}</td>
+        <td onClick={this.toggleEditMode}>Edit</td>
       </tr>
     );
   }
--- app/javascript/journals/index.jsx
+++ app/javascript/journals/index.jsx
@@ -2,21 +2,47 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Row from "./Row"
 
+// Component Constants
+const JOURNAL_HEADERS = ["Title", "Description", "Impact Factor", ""];
+
 class JournalListing extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       journals: this.props.journals,
+      sortDirection: 1,
+    };
+
+    this.setSortDirection = this.setSortDirection.bind(this);
+  }
+
+  /**
+   * Binary flips an integer to be used for journal sorting method
+   */
+  setSortDirection() {
+    const sortDirection = this.state.sortDirection;
+    this.setState({ sortDirection: sortDirection * -1 })
+    return {
+      up: sortDirection * -1,
+      down: sortDirection,
     }
   }
+
+  /**
+   * Sorts a listing of journals in a table
+   * @param  {str} attribute - The named attribute to sort
+   * @return {void}            Updates the journals state
+   */
   sortJournals(attribute) {
+    const currentSortDirection = this.setSortDirection();
+
     this.setState({
       journals:
       this.state.journals.sort((currentJournal, nextJournal) => {
         if (currentJournal[attribute] < nextJournal[attribute]) {
-          return -1;
+          return currentSortDirection['up'];
         } else if (currentJournal[attribute] > nextJournal[attribute]) {
-          return 1;
+          return currentSortDirection['down'];
         }
         return 0;
       })
@@ -25,25 +51,28 @@ class JournalListing extends React.Component {
 
   render() {
     const journals = this.state.journals;
+
     return(
-      <table>
-        <thead>
-          <tr>
-            <th onClick={() => this.sortJournals("title")}>Title</th>
-            <th onClick={() => this.sortJournals("description")}>Description</th>
-            <th onClick={() => this.sortJournals("impact_factor")}>Impact Factor</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {journals.map((journal) =>
-            <Row
-              journal={journal}
-              key={journal.id}
-            />
-          )}
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th onClick={() => this.sortJournals("title")}>Title</th>
+              <th onClick={() => this.sortJournals("description")}>Description</th>
+              <th onClick={() => this.sortJournals("impact_factor")}>Impact Factor</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {journals.map((journal) =>
+              <Row
+                journal={journal}
+                key={journal.id}
+              />
+            )}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
